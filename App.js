7e0908b3210626b7104fe5ceb816c1f70a8861ab
yykdev <assavictory@gmail.com>
2020-08-28 16:01:12 +0900

    앱 로딩시 Asset, font 기본 셋팅

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
 
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
 
 import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
     const [isReady, setIsReady] = useState(false);
 
-    const handleStartAsync = () => {};
-    const handleOnFinish = () => {
-        setIsReady(true);
+    const cachImages = (images) =>
+        images.map((image) => {
+            if (typeof image === "string") {
+                return Image.prefetch(image);
+            } else {
+                return Asset.fromModule(image).downloadAsync();
+            }
+        });
+    const cachFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+    const handleStartAsync = async () => {
+        const images = [
+            // require('./assets/...'),;
+            // "https://.../.../logo.png",
+        ];
+        const fonts = [Ionicons.font];
+
+        const cachImagesPromise = cachImages(images);
+        const cachFontsPromise = cachFonts(fonts);
+
+        return Promise.all([...cachImagesPromise, ...cachFontsPromise]);
     };
+    const handleOnFinish = () => setIsReady(true);
 
     return isReady ? (
         <Text>Ready!!</Text>
