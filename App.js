015c22205ccf298eee62046eb2557cfa4a35841a
yykdev <assavictory@gmail.com>
2020-08-28 16:45:13 +0900

    Provider, PersistGate 셋팅

--- App.js
+++ App.js
@@ -7,6 +7,9 @@ import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import Gate from "./components/Gate";
+import { Provider } from "react-redux";
+import store, { persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App() {
     const [isReady, setIsReady] = useState(false);
@@ -36,7 +39,11 @@ export default function App() {
     const handleOnFinish = () => setIsReady(true);
 
     return isReady ? (
-        <Gate />
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <Gate />
+            </PersistGate>
+        </Provider>
     ) : (
         <AppLoading
             startAsync={handleStartAsync}
