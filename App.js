d00f838d7a9ce1ae7f033d1b103ba495307321ba
yykdev <assavictory@gmail.com>
2020-08-28 16:56:09 +0900

    import 데이터 정렬

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 
-import { Text, Image } from "react-native";
+import { Image } from "react-native";
 
 import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import Gate from "./components/Gate";
+
 import { Provider } from "react-redux";
-import store, { persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./redux/store";
+import Gate from "./components/Gate";
 
 export default function App() {
     const [isReady, setIsReady] = useState(false);
