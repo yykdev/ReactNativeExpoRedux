9eb101d8acaf228ad06c6776d1c0404f5acb8193
yykdev <assavictory@gmail.com>
2020-08-28 15:49:12 +0900

    Expo AppLoading 셋팅

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+
+import { Text } from "react-native";
+
+import { AppLoading } from "expo";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [isReady, setIsReady] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const handleStartAsync = () => {};
+    const handleOnFinish = () => {
+        setIsReady(true);
+    };
+
+    return isReady ? (
+        <Text>Ready!!</Text>
+    ) : (
+        <AppLoading
+            startAsync={handleStartAsync}
+            onError={console.error}
+            onFinish={handleOnFinish}
+        />
+    );
+}
